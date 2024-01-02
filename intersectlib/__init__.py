c957810a8cd22fedbf1ae3a01082da21387f7b20
Rducker0208 <duckerricardo@gmail.com>
2024-01-02 15:47:31 +0100

    Initial commit

---
+++ intersectlib/__init__.py
@@ -0,0 +1,4 @@
+from intersectlib_code import find_intersection
+from intersectlib_code import find_values
+from intersectlib_code import find_remainders
+from intersectlib_code import find_intersection_and_remainders
